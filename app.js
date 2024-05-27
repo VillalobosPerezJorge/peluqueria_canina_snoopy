b3647e4174c788b4864641bce7c96338619bf481
Jorge Villalobos <jorge.villalobos.perez@ciisa.cl>
2024-05-26 21:59:01 -0400

    Corrección fondo

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// MIME para style.css
+app.get('/style.css', function(req, res) {
+  res.set('Content-Type', 'text/css');
+  res.sendFile(__dirname + '/public/style.css');
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
