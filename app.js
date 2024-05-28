ad07dba60118beb5488aae16fbd7b39aa6c6edaf
Jorge Villalobos <jorge.villalobos.perez@ciisa.cl>
2024-05-28 17:20:25 -0400

    Creación páginas base extras
    Se crean páginas para:
    Contacto
    About
    Galería
    Admin
    
    Se corrigen algunos detalles en la navbar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path'); // Importar el módulo 'path'
 const app = express();
 const port = 3000;
 
@@ -11,12 +12,32 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Ruta página de contacto
+app.get('/contacto', (req, res) => {
+  res.render('contact'); 
+});
+
+// Ruta página de galeria
+app.get('/galeria', (req, res) => {
+  res.render('gallery'); 
+});
+
+// Ruta página de contacto
+app.get('/nosotros', (req, res) => {
+  res.render('about'); 
+});
+
+//Ruta página admin
+app.get('/admin', (req, res) => {
+  res.render('admin/admin');
+});
+
+
 // MIME para style.css
 app.get('/style.css', function(req, res) {
   res.set('Content-Type', 'text/css');
   res.sendFile(__dirname + '/public/style.css');
 });
-
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
