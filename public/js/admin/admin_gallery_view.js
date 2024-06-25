0de50d331396f7a9ef2f388db38e802701dacc6e
Jorge Villalobos <jorge.villalobos.perez@ciisa.cl>
2024-06-25 17:53:41 -0400

    Correcciones visuales - About/Index/Services

--- public/js/admin/admin_gallery_view.js
+++ public/js/admin/admin_gallery_view.js
@@ -34,7 +34,7 @@ const cargarPosts = async (windowPage) => {
       </div>
 
       <div class="buttons w-100 d-flex flex-row justify-content-around mt-2 mb-4">
-        <a href="#"><button class="boton_editar btn btn-warning fs-5 fw-bold">Editar</button></a>
+        <a href="/admin_antes"><button class="boton_editar btn btn-warning fs-5 fw-bold">Editar</button></a>
         <button type="button" value='${post._id}' class="boton_eliminar btn btn-danger fs-5 fw-bold">Inhabilitar</button>
       </div>
     </div>`).join('');
