cc76b3bcb9a43b317c9c372062e1903312e4da3c
Jorge Villalobos <jorge.villalobos.perez@ciisa.cl>
2024-06-25 18:10:40 -0400

    Revert "Merge branch 'dev/jorge-villalobos' into develop"
    This reverts commit 3af36f049b1de4ec36fd9e0a00edc04b91454358, reversing
    changes made to 0556ac0ceb701d5e79f54f2b3ff8cb5dc0b9005c.

--- public/js/admin/admin_gallery_view.js
+++ public/js/admin/admin_gallery_view.js
@@ -34,9 +34,8 @@ const cargarPosts = async (windowPage) => {
       </div>
 
       <div class="buttons w-100 d-flex flex-row justify-content-around mt-2 mb-4">
-
-        <a href="/admin_antes"><button class="boton_editar btn btn-warning fs-5 fw-bold">Editar</button></a>
-
+        <a href="#"><button class="boton_editar btn btn-warning fs-5 fw-bold">Editar</button></a>
+        <button type="button" value='${post._id}' class="boton_seleccionar btn btn-success fs-5 fw-bold">Perro del mes</button>
         <button type="button" value='${post._id}' class="boton_eliminar btn btn-danger fs-5 fw-bold">Inhabilitar</button>
       </div>
     </div>`).join('');
