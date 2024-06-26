21b888827033d4c6b3a673cb5b5b636458970738
Vfdezlagos <v.fernandez.lagos@gmail.com>
2024-06-26 16:24:48 -0400

    diferenciador de perro seleccionado añadido; boton cerrar sesion admin arreglado; link sobre nosotros footer arreglado;

--- public/js/general-admin.js
+++ public/js/general-admin.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const nameAdmin = localStorage.getItem('name');
     const surnameAdmin = localStorage.getItem('surname');
       
-      const welcomeMessage = `¡Bienvenido, ${nameAdmin} ${surnameAdmin}! Aquí podrás gestionar los contenidos del sitio web. ¡Que tengas un buen día!`;
-      document.getElementById('welcome-text').textContent = welcomeMessage;
+      // const welcomeMessage = `¡Bienvenido, ${nameAdmin} ${surnameAdmin}! Aquí podrás gestionar los contenidos del sitio web. ¡Que tengas un buen día!`;
+      // document.getElementById('welcome-text').textContent = welcomeMessage;
     if (isLoggedIn) {
         document.getElementById('btn-logout').addEventListener('click', () => {
         localStorage.clear();
