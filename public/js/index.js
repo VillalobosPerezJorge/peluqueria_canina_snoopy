c435428025afa0dac603739f83dd151cd8853f36
Jorge Villalobos <jorge.villalobos.perez@ciisa.cl>
2024-07-01 20:25:16 -0400

    Fixes visuales segun comentarios

--- public/js/index.js
+++ public/js/index.js
@@ -6,58 +6,57 @@ document.addEventListener('DOMContentLoaded', async () => {
     const apiUrl = 'http://18.231.252.59/api/banner/list?active=true';
     const defaultImageUrl = '/public/images/perro1.jpg'; // Ruta de la imagen por defecto
 
+
     try {
         const response = await fetch(apiUrl, {
-        method: 'GET',
-        mode: 'cors',
+            method: 'GET',
+            mode: 'cors',
         });
 
         if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
 
         if (data.status === 'Success' && data.banners && data.banners.banners.length > 0) {
-        data.banners.banners.forEach((banner, index) => {
-            if (index < carouselItems.length) {
-            let imageUrl = defaultImageUrl; // Por defecto, asignar la imagen por defecto
+            data.banners.banners.forEach((banner, index) => {
+                if (index < carouselItems.length) {
+                    let imageUrl = defaultImageUrl; 
 
-            // Verificar si el banner tiene una imagen asignada
-            if (banner.image) {
-                imageUrl = `http://18.231.252.59/api/banner/showimage/${banner._id}`;
+                    if (banner.image) {
+                        imageUrl = `http://18.231.252.59/api/banner/showimage/${banner._id}`;
+                    }
+
+                    const imgElement = carouselItems[index].querySelector('img');
+                    imgElement.src = imageUrl;
+                    imgElement.alt = banner.title;
+
+                    const captionElement = carouselItems[index].querySelector('.carousel-caption');
+                    if (captionElement) {
+                        captionElement.innerHTML = `
+                            <h5>${banner.title}</h5>
+                            <p>${banner.description}</p>
+                        `;
+                    }
+                }
+            });
+
+       
+            const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'), {
+                interval: 5000, 
+                wrap: data.banners.banners.length > 1, 
+            });
+
+          
+            if (data.banners.banners.length === 1) {
+                carouselPrevButton.style.display = 'none';
+                carouselNextButton.style.display = 'none';
             }
-
-            const imgElement = carouselItems[index].querySelector('img');
-            imgElement.src = imageUrl;
-            imgElement.alt = banner.title;
-
-            // Optionally, update carousel captions if needed
-            const captionElement = carouselItems[index].querySelector('.carousel-caption');
-            if (captionElement) {
-                captionElement.innerHTML = `
-                <h5>${banner.title}</h5>
-                <p>${banner.description}</p>
-                `;
-            }
-            }
-        });
-
-        // Activar el carrusel después de cargar las imágenes
-        const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'), {
-            interval: 5000, // Intervalo de 5 segundos
-            wrap: data.banners.banners.length > 1, // Permitir el rebobinado automático solo si hay más de un banner
-        });
-
-        // Deshabilitar las flechas si solo hay un banner activo
-        if (data.banners.banners.length === 1) {
-            carouselPrevButton.style.display = 'none';
-            carouselNextButton.style.display = 'none';
-        }
 
         } else {
-        // Handle case where no active banners are found
-        console.error('No se encontraron banners activos o la estructura de datos es incorrecta.');
+     
+            console.error('No se encontraron banners activos o la estructura de datos es incorrecta.');
         }
     } catch (error) {
         console.error('Error al obtener los banners:', error);
